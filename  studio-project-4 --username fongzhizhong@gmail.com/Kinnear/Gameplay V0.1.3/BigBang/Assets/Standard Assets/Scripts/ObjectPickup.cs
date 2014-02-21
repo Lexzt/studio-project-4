9dce3f93ac7f60d884ce1255e64cba79b161ccb4
vivifira@gmail.com <vivifira@gmail.com@03a76a45-039a-2b4a-a594-2ce638bc6730>
2014-02-21 12:42:54 +0000

    Imp Added. Enemy.

---
+++  studio-project-4 --username fongzhizhong@gmail.com/Kinnear/Gameplay V0.1.3/BigBang/Assets/Standard Assets/Scripts/ObjectPickup.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ObjectPickup : MonoBehaviour {
+
+	public int lengthOfRay;
+	public GameObject holding = null;
+
+	Quaternion instance = Quaternion.identity;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// Shoot a ray from the crosshair?
+		RaycastHit hit;
+		Ray line = new Ray (camera.ScreenToWorldPoint(new Vector3(Screen.width / 2, Screen.height / 2, camera.nearClipPlane)), transform.forward);
+	
+		// Debug line for the raycast
+		Debug.DrawRay (camera.ScreenToWorldPoint(new Vector3(Screen.width / 2, Screen.height / 2, camera.nearClipPlane)), transform.forward * lengthOfRay);
+
+		if(Physics.Raycast(line, out hit, lengthOfRay))
+		{
+			if(hit.collider.tag == "PlayerWeapons")
+			{
+				hit.collider.gameObject.renderer.material.color = Color.blue;
+
+				// Pick up the object
+				if(Input.GetKeyDown(KeyCode.E))
+				{
+					hit.transform.parent.parent = camera.transform;
+					hit.transform.parent.localPosition = new Vector3 (1.0f, -0.2f, 2.0f);
+
+
+
+					instance.eulerAngles = new Vector3(-10, -20, 0);
+					hit.transform.parent.localEulerAngles = instance.eulerAngles;
+					holding = hit.transform.parent.gameObject;
+				}
+			}
+		}
+
+
+
+	}
+}
