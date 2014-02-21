48cd5fc0e45e606ccd3db5cf5817822bd43b5617
vivifira@gmail.com <vivifira@gmail.com@03a76a45-039a-2b4a-a594-2ce638bc6730>
2014-02-21 12:42:54 +0000

    Imp Added. Enemy.

---
+++ trunk/ studio-project-4 --username fongzhizhong@gmail.com/Kinnear/Gameplay V0.1.3/BigBang/Assets/Standard Assets/Scripts/WeaponDamage.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WeaponDamage : MonoBehaviour {
+
+	public bool doesDamage = false;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter(Collider obj)
+	{
+		print("player intersected enemy");
+		
+		// if the player is attacking
+		if(doesDamage)
+		{
+			//detect for enemy collision here
+			if( obj.gameObject.tag == "Enemy")
+			{
+				obj.gameObject.GetComponent<HealthDisplay>().ApplyDamage(10.0f);
+				doesDamage = false;
+			}
+		}
+		
+	}
+}
