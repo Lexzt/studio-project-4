bed861a01daf26ff45a02f45965efc8da09703b1
vivifira@gmail.com <vivifira@gmail.com@03a76a45-039a-2b4a-a594-2ce638bc6730>
2014-02-21 05:30:06 +0000

    Second gameplay implementation. Input in sword example model.

---
+++  studio-project-4 --username fongzhizhong@gmail.com/Kinnear/Gameplay V0.1.1/BigBang/Assets/Standard Assets/Scripts/ObjectDetector.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ObjectDetector : MonoBehaviour {
+
+	GameObject holding = null;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerStay(Collider obj)
+	{
+
+		if(Input.GetKeyDown(KeyCode.E))
+		{
+			if(holding == null)
+			{
+				if( obj.gameObject.tag == "PlayerWeapons")
+				{
+						//Debug.Log(obj.gameObject.name);
+						obj.transform.parent = Camera.main.transform;
+						obj.transform.localPosition = new Vector3 (1.0f, -0.2f, 2.0f);
+						obj.gameObject.rigidbody.isKinematic = true;
+						holding = obj.gameObject;
+				}
+			}
+			else
+			{
+				holding.gameObject.rigidbody.isKinematic = false;
+				holding.transform.parent = null;
+				holding = null;
+			}
+		}
+	}
+}
---
+++  studio-project-4 --username fongzhizhong@gmail.com/Kinnear/Gameplay V0.1.1/BigBang/Assets/Standard Assets/Scripts/ObjectPickup.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ObjectPickup : MonoBehaviour {
+
+	public int lengthOfRay;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// Shoot a ray from the crosshair?
+		RaycastHit hit;
+		Ray line = new Ray (camera.ScreenToWorldPoint(new Vector3(Screen.width / 2, Screen.height / 2, camera.nearClipPlane)), transform.forward);
+	
+		if(Physics.Raycast(line, out hit, lengthOfRay))
+		{
+			if(hit.collider.tag == "PlayerWeapons")
+			{
+				print ("Crosshair over a weapon");
+				hit.collider.gameObject.renderer.material.color = Color.blue;
+			}
+		}
+	
+	
+	}
+}
